57d7c52280d527497408e0a2176e74cc77a6590e
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2021-12-03 17:07:52 +0200

    fullstack react part1 exercise 1.2 complete

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,35 +1,51 @@
 import React from 'react';
 
+const exercises = [10, 7, 14]
+const parts = ["Fundamentals of React :", "Using props to pass data :", "State of a component :"]
 const Header = (prop) =>{
   return (  
     <div>
-      <p>{prop.course}</p>  
+      <p>
+        {prop.course}
+      </p>  
     </div>
   )
 }
 const Part = (prop) =>{
   return(
-    <li>{prop.part}{prop.exercise}</li>
+    <li>
+      {prop.part}{prop.exercise}
+    </li>
   )
 }
 
 const Content = () =>{   
   return ( 
     <div>
-     <Part part={"Fundamentals of React :"} exercise={10}/>
-     <Part part={"Using props to pass data :"} exercise={7}/>
-     <Part part={"State of a component :"} exercise={14}/>
+     <Part part={parts[0]} exercise={exercises[0]}/>
+     <Part part={parts[1]} exercise={exercises[1]}/>
+     <Part part={parts[2]} exercise={exercises[2]}/>
      </div>        
   )
 }
 
-const App = () => {
-  const course = "Fundamental of fuckery";
+const Total = () =>{
+  return(
+    <div>
+      <p> 
+        Number of Exercises : {exercises[0]+exercises[1]+exercises[2]}
+      </p>
+    </div>
+  )
+}
 
+const App = () => {
+  const course = "Half stack application development";
   return (
     <div>
       <Header course={course}/>
-      <Content />       
+      <Content />   
+      <Total/>    
     </div>  
   )
 }
