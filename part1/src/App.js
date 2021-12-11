42dcf516d1a93084e588110fe78fa1d201724fbd
Bishwas Shrestha <bishwas.shrestha@gmail.com>
2021-12-11 02:49:39 +0200

    Introduction to React, Exercises 1.12, 1.13, 1.14 combined

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,38 +1,50 @@
 import React, { useState } from 'react'
 
-
+var array = [0,0,0,0,0,0,0]
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+]
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   const voteArray = [0,0,0,0,0,0,0]
-   const max = anecdotes.length
+ 
+   
   const [selected, setSelected] = useState(0)
   const [clicked, setClick] = useState([])
-console.log(selected)
-  const selectNumber = () =>{
+  const max = anecdotes.length 
+  
+  const selectNumber = () =>{      
     const randomNumber = Math.floor(Math.random()*max)    
     setSelected(randomNumber)      
   }
 
   const voteFunction = () =>{
-  
-  }
-  
-  
+   setClick(array[selected]+=1)
+  }   
+
+  var maxVotes= 0
+  array.forEach(item => {
+    if(item > maxVotes)
+       maxVotes = item;
+  })
+
+var maxVotedQuote = array.indexOf(maxVotes)
+
+console.log(maxVotes, array)
   return (
     <div>         
         <button onClick={selectNumber}> next anecdote </button> 
+        <button onClick={voteFunction}> Vote</button> 
       <h4>
-        {anecdotes[selected]} 
-      </h4>                 
+        <p>{anecdotes[selected]}: has {array[selected]} votes</p>
+      </h4>     
+      <h4>Anecdote with most votes: </h4>            
+      <p>{anecdotes[maxVotedQuote]} has {maxVotes} votes</p>
     </div>    
   )
 }
